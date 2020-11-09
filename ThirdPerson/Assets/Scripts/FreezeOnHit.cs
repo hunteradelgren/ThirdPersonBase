5da26830676b3ef81a492e81ee276a28ea3b0fd0
hunteradelgren <hunteradelgren@gmail.com>
2020-11-09 02:44:04 -0600

    Throw and return works most for the first couple throws. Stops animation of enemy but does not stick.

---
+++ ThirdPerson/Assets/Scripts/FreezeOnHit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeOnHit : MonoBehaviour
+{
+    public Rigidbody rb;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag != "Player")
+        {
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = false;
+            rb.transform.parent = collision.gameObject.transform;
+            if(collision.gameObject.tag == "Enemy")
+            {
+                collision.gameObject.GetComponent<Animator>().StopPlayback();
+            }
+        }
+        
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag !="Player")
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                collision.gameObject.GetComponent<Animator>().StartPlayback();
+            }
+        }
+
+    }
+}
--- ThirdPerson/Assets/Scripts/PlayerController.cs
+++ ThirdPerson/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,8 +15,23 @@ public class PlayerController : MonoBehaviour
     Transform mainCamera;
     [SerializeField]
     BoxCollider swordCollider;
+    [SerializeField]
+    GameObject swordModel;
+
+    public CinemachineFreeLook normal;
+    public CinemachineFreeLook aiming;
+
+
+    public Transform target;
+    public Transform curvepoint;
+    private bool isThrown = false;
+    private bool isReturning = false;
+    private float time = 0.0f;
+    private Vector3 thrownPosition;
+    private Transform initialRotation;
 
     Rigidbody rb;
+    Rigidbody sb;
     Animator anim;
 
     bool startedCombo = false;
@@ -23,13 +40,16 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        sb = swordModel.GetComponent<Rigidbody>();
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        initialRotation = swordModel.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
@@ -48,15 +68,60 @@ public class PlayerController : MonoBehaviour
 
         anim.SetFloat("moveSpeed", Mathf.Abs(moveDirection.magnitude));
 
-        if(Input.GetButtonDown("Jump") && !startedCombo)
+        if(Input.GetButtonDown("Jump") && !startedCombo && (!isThrown && !isReturning))
         {
             anim.SetTrigger("swordCombo");
             startedCombo = true;
         }
 
-        if(Input.GetButtonDown("Jump") && startedCombo)
+        if(Input.GetButtonDown("Jump") && startedCombo && (!isThrown && !isReturning))
         {
             timeSinceButtonPressed = 0;
+        }
+
+        if (Input.GetButton("Aiming"))
+        {
+            aiming.Priority = 25;
+        }
+        else
+        {
+            aiming.Priority = 0;
+        }
+
+        if (Input.GetButton("Aiming")&&Input.GetButtonDown("ThrowAxe"))
+        {
+            if(!isThrown && !isReturning)
+            {
+                Debug.Log("Throw Sword");
+                isThrown = true;
+                anim.SetTrigger("Throw Sword");
+                TurnOnSwordCollider();
+            }
+            else if (isThrown && !isReturning)
+            {
+                Debug.Log("Return Sword");
+                sb.transform.parent = null;
+                isThrown = false;
+                isReturning = true;
+                TurnOffSwordCollider();
+                ReturnSword();
+            }
+
+        }
+
+        //if(isThrown && swordCollider)
+
+        if (isReturning)
+        {
+            if (time < 1)
+            {
+                sb.position = getBQCPoint(time,thrownPosition,curvepoint.position,target.position);
+                time += Time.deltaTime;
+            }
+            else
+            {
+                ResetSword();
+            }
         }
 
         timeSinceButtonPressed += Time.deltaTime;
@@ -91,5 +156,43 @@ public class PlayerController : MonoBehaviour
     {
         swordCollider.enabled = false;
     }
+    public void ThrowSword()
+    {
+        swordModel.transform.parent = null;
+        sb.isKinematic = false;
+        sb.AddForce(Camera.main.transform.TransformDirection(Vector3.forward)*50, ForceMode.Impulse);
+        sb.AddTorque(sb.transform.TransformDirection(Vector3.right) * 100, ForceMode.Impulse);
+    }
 
+    public void SwordThrowHit()
+    {
+        
+    }
+
+    public void ReturnSword()
+    {
+        sb.velocity = Vector3.zero;
+        sb.isKinematic = true;
+        thrownPosition = sb.position;
+    }
+
+    public void ResetSword()
+    {
+        sb.velocity = Vector3.zero;
+        sb.isKinematic = true;
+        isReturning = false;
+        swordModel.transform.parent = target;
+        sb.position = target.position;
+        swordModel.transform.rotation = initialRotation.rotation;
+    }
+
+    //found the sword return in an arc in a tutorial video on youtube
+    Vector3 getBQCPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        Vector3 p = (uu * p0) + (2 * u * t * p1) + (tt * p2);
+        return p;
+    }
 }
